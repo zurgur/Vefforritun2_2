4fd89881f4479fe28b602fa44259459ee83fd3f6
Alexander Freyr Sveinsson <harryfreyr@gmail.com>
2018-02-06 11:46:36 +0000

    form

--- form.js
+++ form.js
@@ -6,7 +6,12 @@ function form(req, res) {
   const data = {};
   res.render('form', { data });
 }
+function register(req, res){
+  console.log("register");
+}
 
 router.get('/', form);
+
+router.post('submit', register);
 
 module.exports = router;
