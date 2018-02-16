f6a85655e64f6a1ea9e21b8086ca27db6904e2c9
zurgur <harryfreyr@gmail.com>
2018-02-16 14:04:01 +0000

    make it better

--- app.js
+++ app.js
@@ -104,8 +104,8 @@ function errorHandler(err, req, res, next) { // eslint-disable-line
 }
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
 });
